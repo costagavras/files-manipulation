fe509f77432b096e4655b44e9e62a0e28f1472ba
costagavras <cgavras@yahoo.ca>
2018-12-22 23:54:51 -0500

    write_file

--- files-manipulation.rb
+++ files-manipulation.rb
@@ -1,32 +1,37 @@
 #code block technique
-File.open("text.txt") do |f|
-  puts f.gets
-end
+# File.open("text.txt") do |f|
+#   puts f.gets
+# end
+#
+# #file handle technique
+# f = File.new("text.txt", "r") #"r" stand for reading
+# puts f.gets
+# f.close
+#
+# #file handle to an instance variable
+# class MyFile
+#   attr_reader :handle
+#
+#   def initialize(filename)
+#     @handle = File.new(filename, "r")
+#   end
+#
+#   def finished
+#     @handle.close
+#   end
+# end
+#
+# f = MyFile.new("text.txt")
+# puts f.handle.gets
+# f.finished
+#
+# File.open("text.txt").each(",") { |line| puts line}
+#
+# File.open("text.txt") do |f|
+#   2.times {puts f.gets}
+# end
 
-#file handle technique
-f = File.new("text.txt", "r") #"r" stand for reading
-puts f.gets
-f.close
-
-#file handle to an instance variable
-class MyFile
-  attr_reader :handle
-
-  def initialize(filename)
-    @handle = File.new(filename, "r")
-  end
-
-  def finished
-    @handle.close
-  end
-end
-
-f = MyFile.new("text.txt")
-puts f.handle.gets
-f.finished
-
-File.open("text.txt").each(",") { |line| puts line}
-
-File.open("text.txt") do |f|
-  2.times {puts f.gets}
+#file write
+File.open("text.txt", "w") do |f|
+  f.puts "This a write-file test"
 end
