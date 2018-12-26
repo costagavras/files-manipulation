daf5fa8394bf163c4c0efc205f7b507786d37cb9
costagavras <cgavras@yahoo.ca>
2018-12-25 20:06:55 -0500

    sqlite db menu

--- sqlite_test.rb
+++ sqlite_test.rb
@@ -53,3 +53,23 @@ def find_person
   }
 
 end
+
+loop do
+  puts %q{Please select an option:
+    1. Create people table
+    2. Add a person
+    3. Look for a person
+    4. Quit}
+
+  case gets.chomp
+    when "1"
+      create_table
+    when "2"
+      add_person
+    when "3"
+      find_person
+    when "4"
+      disconnect_and_quit
+  end
+
+end
